d36370cbf52343ccdd7cae988e1d2bc4ab47fe0b
Joshua Daniel <Joshua_dup@hotmail.com>
2021-06-23 09:50:25 -0500

    Cambio de enrutamiento en index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css'; // Se instala bootstrap en el proyecto y se le hace referencia al archivo css que queremos.
 import './global.css'; //Se importa el archivo css, y las paginas Badgenew y Badges.
+import App  from './components/App'; //Se crea este nuevo componente. Este tendra mi browser router y mis rutas para la single page app.
 // import BadgeNew from './pages/BadgeNew';
 // import Badges from './pages/Badges';
-import App  from './components/App'; //Se crea este nuevo componente. Este tendra mi browser router y mis rutas para la single page app.
+
+import { HashRouter } from 'react-router-dom';
 
 
 //Esta constante guarda el Id(app). Enlaza con el template del index.html
@@ -12,4 +14,9 @@ const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
 // El componente Badge se debe encerrar para que sea un elemento. <>
-ReactDOM.render(<App />, container);
+ReactDOM.render(
+  <HashRouter>
+    <App />
+  </HashRouter>,
+    container);
+  
